215b5c3594a6977de9a942494c49e8f4e6d23c01
vosandr <vosirandr@gmail.com>
2023-11-13 17:20:47 +0300

    Files

--- myclass.c
+++ myclass.c
@@ -16,5 +16,86 @@ struct myclass {
     int value;
 
     //Дочерние классы (если нужны их свойства)
+    t_class1* object1;
+    t_class2* object2;
+
+    t_class1_events* listener_1;
+    t_class2_events* listener_2;
+};
+
+//Обработчики событий объектов дочерних классов
+void class1_handler_event(t_class1* class1_object) {
+    t_myclass* parent = 
+        (t_myclass*) class1_get_parent(class1_object);
+}
+void class2_handler_event(t_class2* class1_object) {
+    t_myclass* parent = 
+        (t_myclass*) class2_get_parent(class1_object);
+}
+
+//Конструктор
+t_myclass* myclass_create(int* exception) {
+    int error;
+
+    //Выделение памяти под основную структуру класса
+    t_myclass* new_object = malloc(sizeof(t_myclass));
+    if(new_object==NULL) {
+        *exception = errno;
+        return NULL;
+    }
+
+    //Инициализация переменных
+    //new_object->value= ;
     
+    //Инициализация дочерних объектов
+    new_object->object1=class1_create(&error);
+    if(new_object->object1 == NULL) {
+        *exception = error;
+        free(new_object);
+        return NULL;
+    }
+
+    new_object->object2 = class2_create(&error);
+
+    //В конструкторе у потомков прописываем родителя
+    class1_set_parent(new_object->object1, (void*)new_object);
+    class2_set_parent(new_object->object2, (void*)new_object);
+
+    return new_object;
+}
+
+//Деструктор
+void myclass_destroy(t_myclass* object) {
+    //Все в обратном порядке
+    class2_destroy(object->object2);
+    class1_destroy(object->object1);
+
+    //free(object->);
+    free(object);
+    object = NULL;
+}
+
+void myclass_set_listener(t_myclass* object, t_my_class_events* listener) 
+{
+    object->listener=listener;
+}
+
+void myclass_set_parent(t_myclass* object, void* parent) 
+{
+    object->parent=parent;
+}
+
+void myclass_get_parent(t_myclass* object) 
+{
+    return object->parent;
+}
+
+void myclass_set_value(t_myclass* object, int value) 
+{
+    object->value=value;
+}
+
+void myclass_get_value(t_myclass* object) 
+{
+    return object->value;
 }
